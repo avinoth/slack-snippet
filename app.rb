2e9c85e4ffac1fd48fbba0b7633dfaa4af35a5b9
Vinoth <a.vinothk@outlook.com>
2015-09-06 11:08:54 +0530

    few fixes, add delete and search actions

--- app.rb
+++ app.rb
@@ -25,14 +25,11 @@ get '/gateway' do
     message += command[:message]
     return message
   end
-
+  title = command[:content][0]
+  snippet = command[:content][1]
   case command[:action]
-    title = command[:content][0]
-    snippet = command[:content][1]
     when :new
-      @snippet = @user.snippets.new
-      @snippet.title = title
-      @snippet.snippet = snippet
+      @snippet = @user.snippets.new({title: title, snippet: snippet})
       message += save_snippet
     when :get
       @snippet = @user.snippets.find_by(title: title)
@@ -42,19 +39,35 @@ get '/gateway' do
         message += @snippet.snippet
       end
     when :edit
-      @snipeet = @user.snippets.find_by(title: title)
+      @snippet = @user.snippets.find_by(title: title)
       unless @snippet
         message += not_found_msg title
       else
         @snippet.snippet = snippet
         message += save_snippet
       end
+    when :delete
+      @snippet = @user.snippets.find_by(title: title)
+      unless @snippet
+        message += not_found_msg title
+      else
+        @snippet.destroy
+        message += "Snippet is successfully destroyed. It is un-recoverable."
+      end
+    when :search
+      query = title
+      results = @user.snippets.where("title ILIKE ?", "%#{query}%")
+      if results
+        message += "Your Search results:- \n " + results.map.with_index {|s, i| "#{i+1}. #{s.title}"}.join("\n")
+      else
+        message += "Your Search query didn't yield any results. Try again with different keywords."
+      end
   end
   message
 end
 
 def extract_content text
-  keywords = {new: 2, get: 1, search: 1, delete: 1, update: 2}
+  keywords = {new: 2, get: 1, edit: 2, delete: 1, search: 1}
   command = text.split('-', 3)
   if command.length < 2
     return {success: false, message: "Not enough arguments."}
@@ -62,21 +75,21 @@ def extract_content text
 
   action = command[0].downcase.to_sym
   if !keywords.keys.include?(action)
-    return {success: false, message: "Invalid Command. Valid commands are `new, get, search, update, delete`"}
+    return {success: false, message: "Invalid Command. Valid commands are `#{keywords.keys.join(", ")}`"}
   elsif keywords[action] != command.length - 1
-    return {success: false, message: "Invalid Format. Valid format are \n `new title`, \n `get title`, \n `search query`, \n `update title content`, \n `delete title`"}
+    return {success: false, message: "Invalid Format. Valid format are \n `new-title-content`, \n `get-title`, \n `search-query`, \n `update-title-content`, \n `delete-title`"}
   else
     return {success: true, action: action, content: command[1..-1]}
   end
 end
 
 def not_found_msg title
-  "Unable to find the snippet #{title}. Check if spelling is correct. You can search for snippets instead by `/snippet search YOUR_QUERY`"
+  "Unable to find #{title}. Check if spelling is correct. You can search for snippets instead by `/snippet search-YOUR_QUERY`"
 end
 
 def save_snippet
   if @snippet.save
-    return "Snippet saved successfully. You can access it by `/snippet get #{@snippet.title}. \n"
+    return "Snippet saved successfully. You can access it by `/snippet get-#{@snippet.title}. \n"
   else
     return "There was some error saving the snippet. Please try again."
   end
