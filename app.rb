7b4b3027cdc7b6ca73faad26ce192979007d2031
Vinoth <a.vinothk@outlook.com>
2015-09-07 09:15:01 +0530

    change all keywords to hyphened characters

--- app.rb
+++ app.rb
@@ -26,28 +26,28 @@ get '/gateway' do
     message += command[:message]
     return message
   end
-  title = command[:content][0]
+  title = command[:content][0].strip
   snippet = command[:content][1]
   case command[:action]
-    when :new
-      @snippet = @user.snippets.new({title: title, snippet: snippet})
+    when '-n'
+      @snippet = @user.snippets.new({title: title, snippet: snippet.strip})
       message += save_snippet
-    when :get
+    when '-g'
       @snippet = @user.snippets.find_by(title: title)
       unless @snippet
         message += not_found_msg title
       else
         message += @snippet.snippet
       end
-    when :edit
+    when '-e'
       @snippet = @user.snippets.find_by(title: title)
       unless @snippet
         message += not_found_msg title
       else
-        @snippet.snippet = snippet
+        @snippet.snippet = snippet.strip
         message += save_snippet
       end
-    when :delete
+    when '-d'
       @snippet = @user.snippets.find_by(title: title)
       unless @snippet
         message += not_found_msg title
@@ -55,7 +55,7 @@ get '/gateway' do
         @snippet.destroy
         message += "Snippet is successfully destroyed. It is un-recoverable."
       end
-    when :search
+    when '-s'
       query = title
       results = @user.snippets.where("title ILIKE ?", "%#{query}%")
       if results
@@ -68,29 +68,29 @@ get '/gateway' do
 end
 
 def extract_content text
-  keywords = {new: 2, get: 1, edit: 2, delete: 1, search: 1}
-  command = text.split('-', 3)
-  if command.length < 2
-    return {success: false, message: "Not enough arguments."}
-  end
+  keywords = {"-n" => 2, "-g" => 1, "-e" => 2, "-d" => 1, "-s" => 1}
+  action = text[0..1]
 
-  action = command[0].downcase.to_sym
   if !keywords.keys.include?(action)
     return {success: false, message: "Invalid Command. Valid commands are `#{keywords.keys.join(", ")}`"}
-  elsif keywords[action] != command.length - 1
-    return {success: false, message: "Invalid Format. Valid format are \n `new-title-content`, \n `get-title`, \n `search-query`, \n `update-title-content`, \n `delete-title`"}
+  end
+
+  command = text.gsub(action, "").strip.split(" -c ")
+
+  if keywords[action] != command.length
+    return {success: false, message: "Invalid Format. Valid format are \n `-n title -c content`, \n `-g title`, \n `-s query`, \n `-e title -c content`, \n `-d title`"}
   else
-    return {success: true, action: action, content: command[1..-1]}
+    return {success: true, action: action, content: command}
   end
 end
 
 def not_found_msg title
-  "Unable to find #{title}. Check if spelling is correct. You can search for snippets instead by `/snippet search-YOUR_QUERY`"
+  "Unable to find #{title}. Check if spelling is correct. You can search for snippets instead by `/snippet -s YOUR_QUERY`"
 end
 
 def save_snippet
   if @snippet.save
-    return "Snippet saved successfully. You can access it by `/snippet get-#{@snippet.title}. \n"
+    return "Snippet saved successfully. You can access it by `/snippet -g #{@snippet.title}. \n"
   else
     return "There was some error saving the snippet. Please try again."
   end
